66806548dd194e0c0b214efff275844c556c0619
Samuel Suh <samsuh@gmail.com>
2020-06-21 02:10:04 -0700

    improve rerender performance using memoized selectors and reselect library

--- src/components/cart-icon/cart-icon.component.js
+++ src/components/cart-icon/cart-icon.component.js
@@ -3,13 +3,14 @@ import { connect } from "react-redux";
 
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 import { toggleCartHidden } from "../../redux/cart/cart.actions";
+import { selectCartItemsCount } from "../../redux/cart/cart.selectors";
 
 import "./cart-icon.styles.scss";
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
   <div className="cart-icon" onClick={toggleCartHidden}>
     <ShoppingIcon className="shopping-icon" />
-    <span className="item-count">0</span>
+    <span className="item-count">{itemCount}</span>
   </div>
 );
 
@@ -17,4 +18,13 @@ const mapDispatchToProps = (dispatch) => ({
   toggleCartHidden: () => dispatch(toggleCartHidden()),
 });
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+// const mapStateToProps = ({ cart: { cartItems } }) => ({
+// itemCount: cartItems.reduce(
+//   (accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity,
+//   0
+// ),
+const mapStateToProps = (state) => ({
+  itemCount: selectCartItemsCount(state),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
---
+++ src/redux/cart/cart.selectors.js
@@ -0,0 +1,31 @@
+import { createSelector } from "reselect";
+
+// //selectors:
+// // 1. input selectors
+// const selectCart = state => state.cart
+// const selectUser = state => state.user
+
+const selectCart = (state) => state.cart;
+
+// // 2. output selector (uses input selectors and createSelector to built itself)
+// export const selectCartItems = createSelector(
+//     [selectCart, selectUser], //a collection of input selectors
+//     (cart, user) => "whatever we output will be output of selector" //a fx that returns the value we want out of the selectors
+// )
+
+//because we used createSelector, it is now a memoized selector
+export const selectCartItems = createSelector(
+  [selectCart],
+  (cart) => cart.cartItems
+);
+
+//gives us back the total quantity in the cart
+export const selectCartItemsCount = createSelector(
+  [selectCartItems],
+  (cartItems) =>
+    cartItems.reduce(
+      (accumulatedQuantity, cartItem) =>
+        accumulatedQuantity + cartItem.quantity,
+      0
+    )
+);
