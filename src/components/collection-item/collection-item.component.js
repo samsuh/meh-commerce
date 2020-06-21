65c15b8a46fee7fff40291af5e1100318f1b00e7
Samuel Suh <samsuh@gmail.com>
2020-06-21 00:13:46 -0700

    implemented cart reducer and populated dropdown with cart-items using cart reducer, also allowed stacking of items in cart for multiple of same item

--- src/components/collection-item/collection-item.component.js
+++ src/components/collection-item/collection-item.component.js
@@ -15,7 +15,7 @@ const CollectionItem = ({ item, addItem }) => {
         <span className="name">{name}</span>
         <span className="price">{price}</span>
       </div>
-      <CustomButton inverted onClick={() => addItem(item)}>
+      <CustomButton inverted="true" onClick={() => addItem(item)}>
         Add to Cart
       </CustomButton>
     </div>
